feeeea982664d5d398444d3e494f7c89dc23be6a
VozzRoman <looper0500@ukr.net>
2022-09-19 17:28:55 +0300

    again

--- src/burger.js
+++ src/burger.js
@@ -1,6 +1,4 @@
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 const buttonOpen = document.querySelector('.button-burger-open');
 const buttonClose = document.querySelector('.button-burger-close');
 const slideDownMenu = document.querySelector('.header__menu');
@@ -14,22 +12,3 @@ buttonClose.addEventListener('click', function () {
 	slideDownMenu.classList.remove('active');
 
 })
-=======
-=======
->>>>>>> Stashed changes
-const burgerOpen = document.querySelector('.button-burger-open');
-
-const burgerClose = document.querySelector('.button-burger-close');
-const burgerMenu = document.querySelector('.header__menu');
-
-burgerOpen.addEventListener('click', function () {
-	burgerMenu.classList.add('active');
-});
-
-burgerClose.addEventListener('click', function () {
-	burgerMenu.classList.remove('active');
-})
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
