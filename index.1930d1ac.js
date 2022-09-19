d807bddb9c6a4271570e4655a910488b94c54988
TarasTikan <109157753+TarasTikan@users.noreply.github.com>
2022-09-19 20:47:10 +0000

    Deploying to gh-pages from @ TarasTikan/team-7@23e1c4d78caba584a0c111fcd151951daf319415 🚀

---
+++ index.1930d1ac.js
@@ -0,0 +1,2 @@
+new Swiper(".myswiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(n,e){return'<span class="'+e+'">'+(n+1)+"</span>"}}});
+//# sourceMappingURL=index.1930d1ac.js.map
---
+++ index.c34703bf.js
@@ -0,0 +1,2 @@
+new Swiper(".myswiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(n,e){return'<span class="'+e+'">'+(n+1)+"</span>"}}});
+//# sourceMappingURL=index.c34703bf.js.map
