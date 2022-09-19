a758d928e193574921f42678c9a7d2b25c77039c
VozzRoman <looper0500@ukr.net>
2022-09-19 17:51:53 +0300

    header/hero/scss

---
+++ src/burger.js
@@ -0,0 +1,14 @@
+
+const buttonOpen = document.querySelector('.button-burger-open');
+const buttonClose = document.querySelector('.button-burger-close');
+const slideDownMenu = document.querySelector('.header__menu');
+
+buttonOpen.addEventListener('click', function () {
+	slideDownMenu.classList.add('active');
+
+})
+
+buttonClose.addEventListener('click', function () {
+	slideDownMenu.classList.remove('active');
+
+})
