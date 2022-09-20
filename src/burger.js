37eeafb24090890fce6975e60d5231045ea8e67a
VozzRoman <looper0500@ukr.net>
2022-09-20 22:31:31 +0300

    addJava

--- src/burger.js
+++ src/burger.js
@@ -4,7 +4,10 @@ const buttonClose = document.querySelector('.button-burger-close');
 const slideDownMenu = document.querySelector('.header__menu');
 
 
-
+const home = document.querySelector('.home__link');
+const howMade = document.querySelector('.homemade__link');
+const products = document.querySelector('.product__link');
+const contact = document.querySelector('.contact__link');
 
 buttonOpen.addEventListener('click', function () {
 	slideDownMenu.classList.add('active');
@@ -16,10 +19,25 @@ buttonClose.addEventListener('click', function (e) {
 	
 })
 
-slideDownMenu.addEventListener('click', function () {
+home.addEventListener('click', function () {
 	slideDownMenu.classList.remove('active');
+	
 })
 
+howMade.addEventListener('click', function () {
+	slideDownMenu.classList.remove('active');
+	
+})
+
+products.addEventListener('click', function () {
+	slideDownMenu.classList.remove('active');
+	
+})
+
+contact.addEventListener('click', function () {
+	slideDownMenu.classList.remove('active');
+	
+})
 
 
 
