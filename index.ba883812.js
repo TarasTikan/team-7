073165e5dc3e476bbb20bf3cc415d747079324ba
TarasTikan <109157753+TarasTikan@users.noreply.github.com>
2022-09-20 19:34:13 +0000

    Deploying to gh-pages from @ TarasTikan/team-7@756d0b6dbbd7798e6e0edbddcd7433fbc6988cb3 🚀

--- index.ba883812.js
+++
@@ -1,2 +0,0 @@
-var buttonOpen=document.querySelector(".button-burger-open"),buttonClose=document.querySelector(".button-burger-close"),slideDownMenu=document.querySelector(".header__menu");buttonOpen.addEventListener("click",(function(){slideDownMenu.classList.add("active")})),buttonClose.addEventListener("click",(function(){slideDownMenu.classList.remove("active")}));
-//# sourceMappingURL=index.ba883812.js.map
