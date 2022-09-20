03f5c4ede415f3f4451b0895add6b72f21ff9cf3
VozzRoman <looper0500@ukr.net>
2022-09-20 21:29:44 +0300

    add/new/correct

--- src/burger.js
+++ src/burger.js
@@ -3,12 +3,23 @@ const buttonOpen = document.querySelector('.button-burger-open');
 const buttonClose = document.querySelector('.button-burger-close');
 const slideDownMenu = document.querySelector('.header__menu');
 
+
+
+
 buttonOpen.addEventListener('click', function () {
 	slideDownMenu.classList.add('active');
 
 })
 
-buttonClose.addEventListener('click', function () {
+buttonClose.addEventListener('click', function (e) {
 	slideDownMenu.classList.remove('active');
-
+	
 })
+
+slideDownMenu.addEventListener('click', function () {
+	slideDownMenu.classList.remove('active');
+})
+
+
+
+
